7342dcb4d7eff8729670b43e9b52f75f78eba47f
gökalp <avcu219@gmail.com>
2022-02-21 16:17:29 +0300

    add chat

--- client/src/context/ChatContext.js
+++ client/src/context/ChatContext.js
@@ -3,7 +3,7 @@ import {createContext, useState} from "react";
 const ChatContext = createContext();
 
 export const ChatProvider = ({children}) => {
-
+const [messages, setMessages] = useState([]);
 
 const values = {
     messages,
